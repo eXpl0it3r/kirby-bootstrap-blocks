8c819c76fdd2ec8c4ae230d6e20c2cf5c1037c87
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2023-05-29 23:37:58 +0200

    Add missing dimissing button

--- snippets/blocks/alert.php
+++ snippets/blocks/alert.php
@@ -1,1 +1,6 @@
-<div class="alert alert-<?= $block->alerttype() ?> <?= $block->dismissible()->toBool() == true ? "alert-dismissible" : "" ?>"><?= $block->text() ?></div>
+<div class="alert alert-<?= $block->alerttype() ?> <?= $block->dismissible()->toBool() == true ? "alert-dismissible" : "" ?>">
+    <?php if ($block->dismissible()->toBool() == true) : ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <?php endif; ?>
+    <?= $block->text() ?>
+</div>
